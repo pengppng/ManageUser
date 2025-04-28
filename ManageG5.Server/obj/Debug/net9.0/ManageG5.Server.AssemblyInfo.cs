72707d2d42669e76c5c8042c508399b32ebf0d55
pengppng <pannaporn.boo@gmail.com>
2025-04-28 13:09:31 +0700

    del Lucide

--- ManageG5.Server/obj/Debug/net9.0/ManageG5.Server.AssemblyInfo.cs
+++ ManageG5.Server/obj/Debug/net9.0/ManageG5.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageG5.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3081c6ddc96b5e046b9126d4c95b7081cb2a5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde5e09c89e257dd6cb031a95ab443d538cbc374")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageG5.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageG5.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
